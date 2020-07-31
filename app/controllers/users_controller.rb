9af42daa581b3ff0dc7c373bc1e2cee466a3d089
bmstyojaop <bmstyojaop@gmail.com>
2020-07-31 22:32:38 +0900

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def like
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @tweets = @user.tweets
+  end
+  
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
