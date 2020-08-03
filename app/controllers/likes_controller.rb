4a3666ac1e748d89a9edf3faf553876512b244cc
bmstyojaop <bmstyojaop@gmail.com>
2020-08-03 18:02:58 +0900

    on hold

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,17 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(tweet_id: params[:tweet_id])
-    redirect_back(fallback_location: root_path)
+    @like = current_user.likes.new(tweet_id: params[:tweet_id])
+    @like.save
+    # redirect_back(fallback_location: root_path)
+    result = [done: "save",user_id: current_user.id, tweet_id: params[:tweet_id]]
+    render :json => result
   end
 
   def destroy
     @like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+    result = [done: "destroy",user_id: current_user.id, tweet_id:params[:tweet_id]]
+    render :json => result
   end
 end
