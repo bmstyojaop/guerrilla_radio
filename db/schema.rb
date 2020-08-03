6ec5401b0235d86075da999e0a3096e2ffae7bfc
bmstyojaop <bmstyojaop@gmail.com>
2020-08-03 19:19:01 +0900

    create migration to intex

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_092625) do
+ActiveRecord::Schema.define(version: 2020_08_03_101416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_092625) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.index ["text"], name: "index_tweets_on_text"
   end
 
   create_table "users", force: :cascade do |t|
