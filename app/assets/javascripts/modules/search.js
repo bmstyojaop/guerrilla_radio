c69876435cb6a2daf342921b97aa1224e65bc09b
bmstyojaop <bmstyojaop@gmail.com>
2020-10-10 19:51:11 +0900

    選択画像ののプレビュー

--- app/assets/javascripts/modules/search.js
+++ app/assets/javascripts/modules/search.js
@@ -82,7 +82,7 @@ $(function() {
         });
       }
       else {
-        appendErrMsgToHTML("一致するツイートがありません");
+        appendErrMsgToHTML("一致する投稿がありません");
       }
     })
     .fail(function() {
