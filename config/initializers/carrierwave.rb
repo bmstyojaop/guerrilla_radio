68957d787e0594d308b6f48ed287bb7677c9b37c
bmstyojaop <bmstyojaop@gmail.com>
2020-11-08 15:11:33 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,33 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["S3_ACCESS_KEY"],
-      aws_secret_access_key: ENV["S3_SECRET_KEY"],
-      region: 'ap-northeast-1'
-    }
-  
-    config.fog_directory  = 'guerrillaradio'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/guerrillaradio'
-  end
-else
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
-    }
-  
-    config.fog_directory  = 'guerrillaradio'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/guerrillaradio'
-  end
-  # CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'guerrillaradio'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/guerrillaradio'
 end
